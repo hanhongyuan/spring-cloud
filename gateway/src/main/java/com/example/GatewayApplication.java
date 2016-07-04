a00eb9fa6b71700d527928c646ac7bf3b3838e57
Joao <Joao>
2016-07-04 19:10:15 -0300

    tests

---
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableZuulProxy
+@RestController
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
